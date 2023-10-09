ed202b50a89915a8511a77729e3fa10d8573cbeb
FireStreaker2 <michaelccc2019@outlook.com>
2023-10-08 20:20:51 -0700

    simple landing page

---
+++ src/components/ui/Header.jsx
@@ -0,0 +1,10 @@
+import Head from "next/head";
+
+export default function Header() {
+	return (
+		<Head>
+			<title>InterlakeClubs</title>
+			<link rel="icon" href="/favicon.ico" />
+		</Head>
+	);
+}
