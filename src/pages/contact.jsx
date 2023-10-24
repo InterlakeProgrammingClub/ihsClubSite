c1bab8ef6535b2505f3503cdd2c84c5ddacf4362
FireStreaker2 <michaelccc2019@outlook.com>
2023-10-23 20:45:48 -0700

    migrate to css variables

--- src/pages/contact.jsx
+++ src/pages/contact.jsx
@@ -21,7 +21,6 @@ export default function Contact() {
 							className={styles.contactOption}
 						>
 							<svg
-								fill="#000000"
 								xmlns="http://www.w3.org/2000/svg"
 								xmlnsXlink="http://www.w3.org/1999/xlink"
 								viewBox="0 0 75.294 75.294"
